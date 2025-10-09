75c4dc475607be6d9cefebc6bf15f7cf17cb7475
JMarie2002 <jessicaregister7@hotmail.com>
2025-10-08 21:26:18 -0400

    ci/cd pipelinewk10

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "w7-jr-terr-bucket321"
-    key          = "week10/terraform.tfstate"
-    region       = "us-east-1"
-    
+    bucket = "w7-jr-terr-bucket321"
+    key    = "week10/terraform.tfstate"
+    region = "us-east-1"
+
   }
 }
